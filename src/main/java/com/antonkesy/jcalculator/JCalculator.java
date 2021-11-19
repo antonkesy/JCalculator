70c4da449f59d26b6bbdaec7cca4f5de10c537bc
Anton Kesy <anton@kesy.de>
2021-11-19 11:48:57 +0100

    add calculate functions

--- src/main/java/com/antonkesy/jcalculator/JCalculator.java
+++ src/main/java/com/antonkesy/jcalculator/JCalculator.java
@@ -1,7 +1,21 @@
 package com.antonkesy.jcalculator;
 
+import com.antonkesy.jcalculator.parser.token.Token;
+import com.antonkesy.jcalculator.parser.Parser;
+
+import java.io.IOException;
+import java.util.List;
+
 public class JCalculator {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static Token calculate(List<Token> expressions) throws IOException {
+        return null;
     }
+
+    public static Token calculate(String expressionString) throws IOException {
+        List<Token> expressions = Parser.convertToExpressions(expressionString);
+        return calculate(expressions);
+    }
+
+
 }
+
--- src/test/java/com/antonkesy/jcalculator/TestJCalculator.java
+++ src/test/java/com/antonkesy/jcalculator/TestJCalculator.java
@@ -1,15 +1,27 @@
 package com.antonkesy.jcalculator;
 
+import com.antonkesy.jcalculator.parser.token.Token;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestJCalculator {
 
+    Token testString(String input) {
+        try {
+            return JCalculator.calculate(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     @Test
-    void addition() {
-        JCalculator calculator = new JCalculator();
-        assertNotNull(calculator);
+    void testEmptyString() {
+        assertNull(testString(""));
+        assertNull(testString("1"));
     }
 
 }
