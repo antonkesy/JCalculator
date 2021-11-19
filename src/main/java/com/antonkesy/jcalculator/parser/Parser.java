fcad0a4363d1558fdcfe3f07dcc847161cbda655
Anton Kesy <anton@kesy.de>
2021-11-19 11:48:38 +0100

    Create Parser.java

---
+++ src/main/java/com/antonkesy/jcalculator/parser/Parser.java
@@ -0,0 +1,12 @@
+package com.antonkesy.jcalculator.parser;
+
+import com.antonkesy.jcalculator.parser.token.Token;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Parser {
+    public static List<Token> convertToExpressions(String expressionList) {
+        return new ArrayList<Token>();
+    }
+}
