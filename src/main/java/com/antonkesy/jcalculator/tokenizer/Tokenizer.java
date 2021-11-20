4433901492168444a5984a66bd05d954b049eec1
Anton Kesy <anton@kesy.de>
2021-11-20 16:50:44 +0100

    Create Tokenizer.java

---
+++ src/main/java/com/antonkesy/jcalculator/tokenizer/Tokenizer.java
@@ -0,0 +1,13 @@
+package com.antonkesy.jcalculator.tokenizer;
+
+import com.antonkesy.jcalculator.token.Token;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tokenizer {
+    public static List<Token> tokenize(String list) throws IOException {
+        return new ArrayList<>();
+    }
+}
