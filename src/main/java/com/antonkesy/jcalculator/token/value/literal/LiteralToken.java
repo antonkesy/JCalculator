840863af079f3f19c5d445ef6986ab4a13bb35a1
Anton Kesy <anton@kesy.de>
2021-11-20 19:31:01 +0100

    create constructor with value

--- src/main/java/com/antonkesy/jcalculator/token/value/literal/LiteralToken.java
+++ src/main/java/com/antonkesy/jcalculator/token/value/literal/LiteralToken.java
@@ -5,6 +5,13 @@ import com.antonkesy.jcalculator.token.value.ValueToken;
 public class LiteralToken implements ValueToken {
     private int value;
 
+    public LiteralToken() {
+    }
+
+    public LiteralToken(int value) {
+        this.value = value;
+    }
+
     @Override
     public int getValue() {
         return value;
