feeddae95c59e59e3e3411c50ba2a41c3eef7695
Anton Kesy <anton@kesy.de>
2021-11-20 18:50:20 +0100

    get all chars to separate with parser

--- src/main/java/com/antonkesy/jcalculator/tokenizer/Tokenizer.java
+++ src/main/java/com/antonkesy/jcalculator/tokenizer/Tokenizer.java
@@ -1,7 +1,11 @@
 package com.antonkesy.jcalculator.tokenizer;
 
 import com.antonkesy.jcalculator.token.Token;
+import com.antonkesy.jcalculator.token.TypeRepresentation;
 import com.antonkesy.jcalculator.token.exception.UnknownTokenException;
+import com.antonkesy.jcalculator.token.operator.OperatorType;
+import com.antonkesy.jcalculator.token.separator.SeparatorType;
+import com.antonkesy.jcalculator.token.value.constant.ConstantType;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,7 +13,21 @@ import java.util.List;
 
 public class Tokenizer {
     public static List<Token> tokenize(String list) throws IOException, UnknownTokenException {
-
+        String[] splitList = list.split("[" + getAllSeparateByChars() + "]");
         return new ArrayList<>();
+    }
+
+    private static String enumRepresentations(TypeRepresentation[] enumValues) {
+        StringBuilder allReps = new StringBuilder(enumValues.length);
+        for (TypeRepresentation value : enumValues) {
+            allReps.append(value.getTypeRepresentation());
+        }
+        return allReps.toString();
+    }
+
+    private static String getAllSeparateByChars() {
+        return enumRepresentations(SeparatorType.values()) +
+                enumRepresentations(ConstantType.values()) +
+                enumRepresentations(OperatorType.values());
     }
 }
