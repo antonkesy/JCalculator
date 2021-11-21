80cdc3a84d6e0f8eeeece255b1af49f8539eb611
Anton Kesy <anton@kesy.de>
2021-11-21 17:51:46 +0100

    add message constructor

--- src/main/java/com/antonkesy/jcalculator/token/exception/UnknownTokenException.java
+++ src/main/java/com/antonkesy/jcalculator/token/exception/UnknownTokenException.java
@@ -3,4 +3,12 @@ package com.antonkesy.jcalculator.token.exception;
 import java.io.IOException;
 
 public class UnknownTokenException extends IOException {
+
+    public UnknownTokenException() {
+        super();
+    }
+
+    public UnknownTokenException(String stringToken) {
+        super(stringToken);
+    }
 }
