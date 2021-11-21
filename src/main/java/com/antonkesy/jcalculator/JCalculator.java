39e2b227f67c94a882faa12ef3d72d9805c98d6a
Anton Kesy <anton@kesy.de>
2021-11-21 18:33:35 +0100

    specify exception

--- src/main/java/com/antonkesy/jcalculator/JCalculator.java
+++ src/main/java/com/antonkesy/jcalculator/JCalculator.java
@@ -3,16 +3,16 @@ package com.antonkesy.jcalculator;
 import com.antonkesy.jcalculator.tokenizer.token.Token;
 import com.antonkesy.jcalculator.parser.Parser;
 import com.antonkesy.jcalculator.tokenizer.Tokenizer;
+import com.antonkesy.jcalculator.tokenizer.token.exception.UnknownTokenException;
 
-import java.io.IOException;
 import java.util.List;
 
 public class JCalculator {
-    public static Token calculate(List<Token> tokenList) throws IOException {
+    public static Token calculate(List<Token> tokenList) throws UnknownTokenException {
         return null;
     }
 
-    public static Token calculate(String expressionString) throws IOException {
+    public static Token calculate(String expressionString) throws UnknownTokenException {
         List<Token> expressions = Parser.convertToExpressions(Tokenizer.tokenize(expressionString));
         return calculate(expressions);
     }
