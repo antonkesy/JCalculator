fb7a2b1f70e1204dbfa3ad256c7e2d36cc78588a
Anton Kesy <anton@kesy.de>
2021-11-21 18:57:36 +0100

    Update JCalculator.java

--- src/main/java/com/antonkesy/jcalculator/JCalculator.java
+++ src/main/java/com/antonkesy/jcalculator/JCalculator.java
@@ -1,20 +1,21 @@
 package com.antonkesy.jcalculator;
 
-import com.antonkesy.jcalculator.tokenizer.token.Token;
 import com.antonkesy.jcalculator.parser.Parser;
+import com.antonkesy.jcalculator.parser.ast_nodes.Node;
+import com.antonkesy.jcalculator.tokenizer.token.Token;
 import com.antonkesy.jcalculator.tokenizer.Tokenizer;
 import com.antonkesy.jcalculator.tokenizer.token.exception.UnknownTokenException;
 
 import java.util.List;
 
 public class JCalculator {
-    public static Token calculate(List<Token> tokenList) throws UnknownTokenException {
+    public static Token calculate(List<Token> tokenList) {
+        Node rootAst = Parser.buildAST(tokenList);
         return null;
     }
 
     public static Token calculate(String expressionString) throws UnknownTokenException {
-        List<Token> expressions = Parser.convertToExpressions(Tokenizer.tokenize(expressionString));
-        return calculate(expressions);
+        return calculate(Tokenizer.tokenize(expressionString));
     }
 
 
