5c499478794d16c1d3dc5bcd09521f122b9ca3c0
Anton <anton@kesy.de>
2021-12-04 18:00:40 +0100

    update naive recursive looking forward parser

--- src/main/java/com/antonkesy/jcalculator/parser/Parser.java
+++ src/main/java/com/antonkesy/jcalculator/parser/Parser.java
@@ -3,55 +3,71 @@ package com.antonkesy.jcalculator.parser;
 import com.antonkesy.jcalculator.parser.ast_nodes.ExpressionNode;
 import com.antonkesy.jcalculator.parser.ast_nodes.FactorNode;
 import com.antonkesy.jcalculator.parser.ast_nodes.Node;
-import com.antonkesy.jcalculator.parser.double_linked_token.DoubleLinkedToken;
+import com.antonkesy.jcalculator.parser.ast_nodes.TermNode;
 import com.antonkesy.jcalculator.tokenizer.token.Token;
+import com.antonkesy.jcalculator.tokenizer.token.operator.OperatorToken;
 import com.antonkesy.jcalculator.tokenizer.token.value.ValueToken;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class Parser {
-    public static Node buildAST(List<Token> tokens) {
-        if (tokens.isEmpty()) return null;
-        Node lastNode = buildExpression(null, buildDoubleLinkedTokenList(tokens));
-        return getRootNode(lastNode);
+    private int tokenIndex = -1;
+    private final Token[] token;
+    private Node lastNode;
+    private Node root;
+
+    public Parser(List<Token> tokens) {
+        this.token = tokens.toArray(new Token[0]);
+        buildAST();
     }
 
-
-    public static Node buildExpression(Node lastNode, DoubleLinkedToken tokenToAdd) {
-        //TODO return root not last node but root!
-        if (tokenToAdd == null) return lastNode;
-        //terminal
-        if (tokenToAdd.value instanceof ValueToken) {
-            return buildExpression(new FactorNode(tokenToAdd.value, lastNode), tokenToAdd.after);
-        }
-        /*
-        if(tokenToAdd instanceof SeparatorToken){
-           //TODO add separator
-        }*/
-        //non-terminal
-        return new ExpressionNode(tokenToAdd.value, null, lastNode, buildExpression(lastNode, tokenToAdd.after));
+    public Node getRootNode() {
+        return root;
     }
 
-    public static DoubleLinkedToken buildDoubleLinkedTokenList(List<Token> tokenList) {
-        DoubleLinkedToken start = null;
-        DoubleLinkedToken last = null;
-        for (Token token : tokenList) {
-            if (start == null) {
-                start = last;
+    private void buildAST() {
+        //TODO not working if last is double term token
+        Node result;
+        do {
+            result = parseNextToken();
+            if (result != null) {
+                if (lastNode == null) {
+                    lastNode = result;
+                }
+                root = result;
             }
-            DoubleLinkedToken newLinkedToken = new DoubleLinkedToken(token, last);
-            if (last != null) {
-                last.after = newLinkedToken;
-            }
-            last = newLinkedToken;
-        }
-        return start;
+        } while (result != null);
     }
 
-    private static Node getRootNode(Node lastNode) {
-        if (lastNode.parent != null) {
-            return getRootNode(lastNode.parent);
+    private Node parseNextToken() {
+        Token currentToken = getNextToken();
+        if (currentToken instanceof ValueToken) {
+            return new FactorNode(currentToken);
+        } else if (currentToken instanceof OperatorToken && lastNode != null) {
+            Node copyLastNode = lastNode;
+            Node exp = new ExpressionNode(currentToken, copyLastNode, parseNextToken());
+            lastNode = exp;
+            return exp;
         }
-        return lastNode;
+        //TODO add all token
+        return null;
     }
+
+
+    private Token getNextToken() {
+        ++tokenIndex;
+        return getCurrentToken();
+    }
+
+    private Token getCurrentToken() {
+        if (isTokenIndexInBound())
+            return token[tokenIndex];
+        else return null;
+    }
+
+    private boolean isTokenIndexInBound() {
+        return tokenIndex < token.length;
+    }
+
 }
