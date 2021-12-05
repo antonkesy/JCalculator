90100a95cdd648c2959cb0c2c1fe7e4cce95488b
Anton <anton@kesy.de>
2021-12-05 15:16:36 +0100

    Update JCalculator.java

--- src/main/java/com/antonkesy/jcalculator/JCalculator.java
+++ src/main/java/com/antonkesy/jcalculator/JCalculator.java
@@ -2,6 +2,7 @@ package com.antonkesy.jcalculator;
 
 import com.antonkesy.jcalculator.parser.Parser;
 import com.antonkesy.jcalculator.parser.ast_nodes.Node;
+import com.antonkesy.jcalculator.parser.exception.MissingTokenException;
 import com.antonkesy.jcalculator.tokenizer.token.Token;
 import com.antonkesy.jcalculator.tokenizer.Tokenizer;
 import com.antonkesy.jcalculator.tokenizer.token.exception.UnknownTokenException;
@@ -11,17 +12,17 @@ import java.util.List;
 public class JCalculator {
     //TODO create calculator object, non static
     //TODO override list of constants
-    public static Token calculate(Tokenizer tokenizer) {
+    public static Token calculate(Tokenizer tokenizer) throws MissingTokenException {
         Parser parser = new Parser(tokenizer);
         Node rootAst = parser.getRootNode();
         return null;
     }
 
-    public static Token calculate(String expressionString) throws UnknownTokenException {
+    public static Token calculate(String expressionString) throws UnknownTokenException, MissingTokenException {
         return calculate(new Tokenizer(expressionString));
     }
 
-    public static Token calculate(List<Token> tokenList) throws UnknownTokenException {
+    public static Token calculate(List<Token> tokenList) throws MissingTokenException {
         Tokenizer tokenizer = new Tokenizer(tokenList);
         return calculate(tokenizer);
     }
