2448a55d4c4eae363cc742a8a56722af22f9a8c4
Anton <anton@kesy.de>
2021-12-05 15:43:33 +0100

    add calculation of ast

--- src/main/java/com/antonkesy/jcalculator/JCalculator.java
+++ src/main/java/com/antonkesy/jcalculator/JCalculator.java
@@ -1,31 +1,64 @@
 package com.antonkesy.jcalculator;
 
 import com.antonkesy.jcalculator.parser.Parser;
+import com.antonkesy.jcalculator.parser.ast_nodes.ExpressionNode;
+import com.antonkesy.jcalculator.parser.ast_nodes.FactorNode;
 import com.antonkesy.jcalculator.parser.ast_nodes.Node;
 import com.antonkesy.jcalculator.parser.exception.MissingTokenException;
 import com.antonkesy.jcalculator.tokenizer.token.Token;
 import com.antonkesy.jcalculator.tokenizer.Tokenizer;
 import com.antonkesy.jcalculator.tokenizer.token.exception.UnknownTokenException;
+import com.antonkesy.jcalculator.tokenizer.token.operator.OperatorToken;
+import com.antonkesy.jcalculator.tokenizer.token.value.ValueToken;
 
 import java.util.List;
 
 public class JCalculator {
-    //TODO create calculator object, non static
-    //TODO override list of constants
-    public static Token calculate(Tokenizer tokenizer) throws MissingTokenException {
+    public static int calculate(Tokenizer tokenizer) throws MissingTokenException {
         Parser parser = new Parser(tokenizer);
         Node rootAst = parser.getRootNode();
-        return null;
+        return calculateAst(rootAst);
     }
 
-    public static Token calculate(String expressionString) throws UnknownTokenException, MissingTokenException {
+    public static int calculate(String expressionString) throws UnknownTokenException, MissingTokenException {
         return calculate(new Tokenizer(expressionString));
     }
 
-    public static Token calculate(List<Token> tokenList) throws MissingTokenException {
+    public static int calculate(List<Token> tokenList) throws MissingTokenException {
         Tokenizer tokenizer = new Tokenizer(tokenList);
         return calculate(tokenizer);
     }
 
+    private static int calculateAst(Node astNode) {
+        if (astNode == null) {
+            return 0;
+        } else if (astNode instanceof FactorNode) {
+            return ((ValueToken) astNode.token).getValue();
+        } else if (astNode instanceof ExpressionNode) {
+            return calculateExpression((ExpressionNode) astNode);
+        }
+        return 0;
+    }
+
+    private static int calculateExpression(ExpressionNode node) {
+        int result = 0;
+        int leftValue = calculateAst(node.leftChild);
+        int rightValue = calculateAst(node.rightChild);
+        switch (((OperatorToken) node.token).operator) {
+            case ADD:
+                result = leftValue + rightValue;
+                break;
+            case SUB:
+                result = leftValue - rightValue;
+                break;
+            case MULTIPLY:
+                result = leftValue * rightValue;
+                break;
+            case DIVIDE:
+                result = leftValue / rightValue;
+                break;
+        }
+        return result;
+    }
 }
 
