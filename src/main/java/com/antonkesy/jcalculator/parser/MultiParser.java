594523b8b49a9ed65d0254ffb819cf4935f315e7
Anton <anton@kesy.de>
2021-12-08 18:12:58 +0100

    Create MultiParser.java

---
+++ src/main/java/com/antonkesy/jcalculator/parser/MultiParser.java
@@ -0,0 +1,26 @@
+package com.antonkesy.jcalculator.parser;
+
+import com.antonkesy.jcalculator.tokenizer.token.Token;
+import com.antonkesy.jcalculator.tokenizer.token.operator.OperatorToken;
+import com.antonkesy.jcalculator.tokenizer.token.operator.OperatorType;
+
+public class MultiParser extends PartialParser {
+
+    protected MultiParser(IParser nextHigherPartialParser) {
+        super(nextHigherPartialParser);
+    }
+
+    @Override
+    protected boolean isSameType(Token token) {
+        OperatorToken opTok = (OperatorToken) token;
+        for (OperatorType type : new OperatorType[]{OperatorType.MULTIPLY, OperatorType.DIVIDE}) {
+            if (type == opTok.operator) return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean isInstanceOf(Token token) {
+        return token instanceof OperatorToken;
+    }
+}
