c75fed7658b9ba91ed68941902e30f5da281fb24
Anton <anton@kesy.de>
2021-12-12 16:51:16 +0100

    fix recursive loop

--- src/main/java/com/antonkesy/jcalculator/parser/ParenthesesParser.java
+++ src/main/java/com/antonkesy/jcalculator/parser/ParenthesesParser.java
@@ -1,33 +1,33 @@
 package com.antonkesy.jcalculator.parser;
 
 import com.antonkesy.jcalculator.parser.ast_nodes.Node;
+import com.antonkesy.jcalculator.tokenizer.Tokenizer;
 import com.antonkesy.jcalculator.tokenizer.token.Token;
 import com.antonkesy.jcalculator.tokenizer.token.separator.SeparatorToken;
 import com.antonkesy.jcalculator.tokenizer.token.separator.SeparatorType;
 
 public class ParenthesesParser implements IParser {
 
+    Tokenizer tokenizer;
     private final IParser rootParser;
     private final IParser nextHigher;
 
-    protected ParenthesesParser(IParser rootParser, IParser nextHigher) {
+    protected ParenthesesParser(IParser rootParser, IParser nextHigher, Tokenizer tokenizer) {
         this.rootParser = rootParser;
         this.nextHigher = nextHigher;
+        this.tokenizer = tokenizer;
     }
 
     @Override
     public Node parse() {
         Node left = nextHigher.parse();
         if (left != null && isSeparatorType(left.token, SeparatorType.OPEN)) {
-            Node inside;
-            Node right = null;
-            while (true) {
-                inside = right;
-                right = rootParser.parse();
-                if (right != null && isSeparatorType(right.token, SeparatorType.CLOSE)) {
-                    return inside;
-                }
+            Node inside = null;
+            while (!isSeparatorType(tokenizer.peek(), SeparatorType.CLOSE)) {
+                inside = rootParser.parse();
             }
+            tokenizer.nextToken();
+            return inside;
         }
         return left;
     }
