e9c8ec64ccf41977cd4ec58244d044be01f2f45b
Anton <anton@kesy.de>
2021-12-12 16:27:36 +0100

    increase readability

--- src/main/java/com/antonkesy/jcalculator/parser/ParenthesesParser.java
+++ src/main/java/com/antonkesy/jcalculator/parser/ParenthesesParser.java
@@ -1,10 +1,7 @@
 package com.antonkesy.jcalculator.parser;
 
 import com.antonkesy.jcalculator.parser.ast_nodes.Node;
-import com.antonkesy.jcalculator.tokenizer.Tokenizer;
 import com.antonkesy.jcalculator.tokenizer.token.Token;
-import com.antonkesy.jcalculator.tokenizer.token.operator.OperatorToken;
-import com.antonkesy.jcalculator.tokenizer.token.operator.OperatorType;
 import com.antonkesy.jcalculator.tokenizer.token.separator.SeparatorToken;
 import com.antonkesy.jcalculator.tokenizer.token.separator.SeparatorType;
 
@@ -21,17 +18,21 @@ public class ParenthesesParser implements IParser {
     @Override
     public Node parse() {
         Node left = nextHigher.parse();
-        if (left.token instanceof SeparatorToken && ((SeparatorToken) (left.token)).separatorType == SeparatorType.OPEN) {
+        if (left != null && isSeparatorType(left.token, SeparatorType.OPEN)) {
             Node inside;
             Node right = null;
             while (true) {
                 inside = right;
                 right = rootParser.parse();
-                if (right != null && right.token instanceof SeparatorToken && ((SeparatorToken) right.token).separatorType == SeparatorType.CLOSE) {
+                if (right != null && isSeparatorType(right.token, SeparatorType.CLOSE)) {
                     return inside;
                 }
             }
         }
         return left;
+    }
+
+    private boolean isSeparatorType(Token token, SeparatorType type) {
+        return token instanceof SeparatorToken && ((SeparatorToken) token).separatorType == type;
     }
 }
