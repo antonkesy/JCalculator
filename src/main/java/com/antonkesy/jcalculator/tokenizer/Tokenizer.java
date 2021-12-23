3d03b65eaba7473986061c07491ab8711f27d56e
Anton <anton@kesy.de>
2021-12-23 13:42:52 +0100

    simplify tokenizer

--- src/main/java/com/antonkesy/jcalculator/tokenizer/Tokenizer.java
+++ src/main/java/com/antonkesy/jcalculator/tokenizer/Tokenizer.java
@@ -3,86 +3,76 @@ package com.antonkesy.jcalculator.tokenizer;
 import com.antonkesy.jcalculator.tokenizer.token.Token;
 import com.antonkesy.jcalculator.tokenizer.token.TypeRepresentation;
 import com.antonkesy.jcalculator.tokenizer.exception.UnknownTokenException;
+import com.antonkesy.jcalculator.tokenizer.token.operator.OperatorToken;
 import com.antonkesy.jcalculator.tokenizer.token.operator.OperatorType;
 import com.antonkesy.jcalculator.tokenizer.token.separator.SeparatorType;
+import com.antonkesy.jcalculator.tokenizer.token.value.ValueToken;
 import com.antonkesy.jcalculator.tokenizer.token.value.constant.ConstantType;
 import com.antonkesy.jcalculator.tokenizer.token.value.literal.LiteralToken;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class Tokenizer {
     private int tokenIndex = -1;
-    private final List<Token> token;
+    private List<Token> token;
 
     public Tokenizer(String input) throws UnknownTokenException {
-        token = tokenize(input);
+        tokenize(input);
     }
 
     public Tokenizer(List<Token> alreadyTokenized) {
         this.token = alreadyTokenized;
     }
 
-    protected static List<Token> tokenize(String input) throws UnknownTokenException {
-        if (input.isEmpty()) throw new UnknownTokenException();
-        ArrayList<Token> tokenList = new ArrayList<>();
-        //split input token input by delimiters
-        for (String stringToken : addSpacesDelimiter(input).split("\\s")) {
-            //skip empty or only space token
-            if (stringToken.isEmpty() || stringToken.matches("\\s*")) continue;
-            //add token if exists
-            tokenList.add(getTokenFromString(stringToken));
-        }
-        if (tokenList.isEmpty()) throw new UnknownTokenException();
-        return tokenList;
+    public List<Token> getToken() {
+        return this.token;
     }
 
-    protected static Token getTokenFromString(String stringToken) throws UnknownTokenException {
+    private void tokenize(String input) throws UnknownTokenException {
+        if (input.isEmpty()) throw new UnknownTokenException();
+        token = new LinkedList<>();
+        StringBuilder bufferedToken = new StringBuilder();
+        Token lastAddedToken = null;
+        Token lastPossibleToken = null;
+        //TODO with next
+        for (int i = 0; i < input.length(); ++i) {
+            //skip spaces
+            if (input.charAt(i) == ' ') continue;
+            bufferedToken.append(input.charAt(i));
+            Token currentPossibleToken = getTokenFromString(bufferedToken.toString(), lastAddedToken);
+            //if current token is not possible then use last possible
+            if (currentPossibleToken == null && lastPossibleToken != null) {
+                lastAddedToken = lastPossibleToken;
+                token.add(lastAddedToken);
+                lastPossibleToken = null;
+                bufferedToken.setLength(0);
+                --i;
+            } else {
+                lastPossibleToken = currentPossibleToken;
+            }
+        }
+        //add rest
+        if (lastPossibleToken != null) {
+            token.add(lastPossibleToken);
+        } else {
+            throw new UnknownTokenException();
+        }
+    }
+
+    private Token getTokenFromString(String stringToken, Token lastToken) {
         Token token;
         //check token of types
-        if ((token = getTokenOfType(stringToken)) != null) return token;
+        if ((token = getTokenOfType(stringToken)) != null)
+            return token;
         //check literal token
-        if ((token = getLiteralToken(stringToken)) != null) return token;
-        throw new UnknownTokenException(stringToken);
+        if (!(lastToken instanceof ValueToken) && (token = getLiteralToken(stringToken)) != null)
+            return token;
+        return null;
     }
 
-    private static String enumRepresentations(TypeRepresentation[] enumValues) {
-        StringBuilder allReps = new StringBuilder(enumValues.length);
-        for (TypeRepresentation value : enumValues) {
-            allReps.append(value.getTypeRepresentation()).append(' ');
-        }
-        //remove last space
-        allReps.deleteCharAt(allReps.length() - 1);
-        return allReps.toString();
-    }
-
-    private static String getAllSeparateByChars() {
-        return enumRepresentations(
-                SeparatorType.values()) + ' ' +
-                enumRepresentations(ConstantType.values()) + ' ' +
-                enumRepresentations(OperatorType.values()) + ' ';
-    }
-
-    /**
-     * @param optionString split by space all options regex should use
-     * @return "(optionA|optionB|optionN)"
-     */
-    protected static String getOptionRegex(String optionString) {
-        StringBuilder regex = new StringBuilder();
-        //add every char as regex option
-        regex.append('(');
-        for (String s : optionString.split("\\s")) {
-            regex.append(Pattern.quote(s)).append('|');
-        }
-        //remove last appended '|'
-        regex.deleteCharAt(regex.length() - 1);
-        //end of regex option
-        regex.append(')');
-        return regex.toString();
-    }
-
-    protected static Token getTokenOfType(String tokenString) {
+    private Token getTokenOfType(String tokenString) {
         for (TypeRepresentation[] types : getAllTypes()) {
             for (TypeRepresentation option : types) {
                 if (tokenString.matches(Pattern.quote(option.getTypeRepresentation()))) {
@@ -93,23 +83,16 @@ public class Tokenizer {
         return null;
     }
 
-    protected static TypeRepresentation[][] getAllTypes() {
+
+    private TypeRepresentation[][] getAllTypes() {
         return new TypeRepresentation[][]{OperatorType.values(), ConstantType.values(), SeparatorType.values()};
     }
 
-    protected static Token getLiteralToken(String tokenString) {
+    private Token getLiteralToken(String tokenString) {
         if (tokenString.matches("([-+])*[0-9]+")) {
             return new LiteralToken(Integer.parseInt(tokenString));
         }
         return null;
-    }
-
-    protected static String addSpacesDelimiter(String input) {
-        String[] delimiters = getAllSeparateByChars().split("\\s");
-        for (String delimiter : delimiters) {
-            input = input.replace(delimiter, " " + delimiter + " ");
-        }
-        return input;
     }
 
     public Token peek() {
