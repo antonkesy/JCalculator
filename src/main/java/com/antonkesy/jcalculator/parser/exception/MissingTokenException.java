4d5075a8d6b8e1013fe877eb2b9667692c947945
Anton <anton@kesy.de>
2021-12-24 17:32:22 +0100

    Update MissingTokenException.java

--- src/main/java/com/antonkesy/jcalculator/parser/exception/MissingTokenException.java
+++ src/main/java/com/antonkesy/jcalculator/parser/exception/MissingTokenException.java
@@ -3,11 +3,12 @@ package com.antonkesy.jcalculator.parser.exception;
 import com.antonkesy.jcalculator.tokenizer.token.Token;
 
 public class MissingTokenException extends Exception {
-    public MissingTokenException() {
-        super();
-    }
-
     public MissingTokenException(Token tokenExpected) {
         super(tokenExpected.getRepresentation() + " not found");
     }
+
+    public MissingTokenException(String tokenName) {
+        super(tokenName + " not found");
+    }
+
 }
