ef8792b92c8b57005fd69b57ab5271076bb1b2bb
Anton Kesy <anton@kesy.de>
2022-02-08 15:52:06 +0100

    use INumber interface

--- src/main/java/com/antonkesy/jcalculator/tokenizer/token/value/ValueToken.java
+++ src/main/java/com/antonkesy/jcalculator/tokenizer/token/value/ValueToken.java
@@ -1,9 +1,8 @@
 package com.antonkesy.jcalculator.tokenizer.token.value;
 
+import com.antonkesy.jcalculator.number.INumber;
 import com.antonkesy.jcalculator.tokenizer.token.Token;
 
-import java.math.BigDecimal;
-
 public interface ValueToken extends Token {
-    BigDecimal getValue();
+    INumber getValue();
 }
